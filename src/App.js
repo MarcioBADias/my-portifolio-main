831a7ad236396e3e27ccd658731bf731f2b23f73
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-03-08 16:41:56 -0300

    Ajuste na navegação com as rotas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Header from "./components/header";
+import Projects from "./components/projects";
+import Skills from "./components/skills";
 import Home from "./pages/home";
 import Global from "./styles/global";
 
 function App() {
   return (
-    <div>
-      <Home />
-      <Global />
-    </div>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+          <Route path="/" element={<Navigate to="/projects" />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/" element={<Navigate to="/skills" />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" exact element={<Home />} />
+      </Routes>
+      <Global/>
+    </BrowserRouter>
   );
 }
 
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,23 +1,15 @@
 import React from "react";
-import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
-import Header from "../../components/header";
 import Resume from '../../components/resume';
 import Skills from '../../components/skills';
 import Projects from '../../components/projects';
 
 const Home = () => {
     return(
-        <BrowserRouter>
-            <Header/>
-            <Routes>
-                <Route path="/" element={<Navigate to="/projects" />} />
-                <Route path="/projects" element={<Projects />} />
-                <Route path="/" element={<Navigate to="/skills" />} />
-                <Route path="/skills" element={<Skills />} />
-                <Route path="/" element={<Navigate to="/home" />} />
-                <Route path="/home" exact element={<Resume />} />
-            </Routes>
-        </BrowserRouter>
+        <div>
+            <Resume/>
+            <Skills/>
+            <Projects/>
+        </div>
     )
 }
 
