7867b07562ac1bda535c0d1ac476494e75b37f88
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-03-08 16:51:01 -0300

    Esbolço do Footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,30 @@
+i/*mport React from "react";
+
+import * as C from './style'
+
+const Footer = () => {
+    return(
+        <C.Section>
+            <C.Container>
+                <C.Title>
+                    Contatos
+                </C.Title>
+                <C.Adress>
+
+                </C.Adress>
+            </C.Container>
+            <C.Container>
+                <C.Logo/>
+            </C.Container>
+            <C.Container>
+                <C.Title>
+                    <C.SocialIcons></C.SocialIcons>
+                    <C.SocialIcons></C.SocialIcons>
+                    <C.SocialIcons></C.SocialIcons>
+                </C.Title>
+            </C.Container>
+        </C.Section>
+    )
+}
+
+export default Footer;*/
