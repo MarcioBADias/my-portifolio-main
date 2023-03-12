aa22fcee7602a369ba8942f93f552a545daf9ed4
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-03-12 20:07:30 -0300

    Atualizando rotas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/header";
 import Projects from "./components/projects";
 import Skills from "./components/skills";
@@ -11,12 +11,9 @@ function App() {
     <BrowserRouter>
       <Header/>
       <Routes>
-          <Route path="/" element={<Navigate to="/projects" />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/" element={<Navigate to="/skills" />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/" element={<Navigate to="/home" />} />
           <Route path="/home" exact element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/skills" element={<Skills />} />
       </Routes>
       <Global/>
     </BrowserRouter>
